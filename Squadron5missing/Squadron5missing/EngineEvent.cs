f4e4af4ab2a7ed3e915116a9872a00c5b8a736e9
Luuken <LuukMaan@outlook.com>
2016-01-28 09:06:23 +0100

    MotorEvent.2
    Ja

--- Squadron5missing/Squadron5missing/EngineEvent.cs
+++ Squadron5missing/Squadron5missing/EngineEvent.cs
@@ -5,7 +5,16 @@ using System.Text;
 
 namespace Squadron5missing
 {
-    class EngineEvent
+    class EngineEvent : Event
     {
+        //properties
+        protected int EngineRepaired { get; set; }
+
+        //constructor(s)
+        public EngineEvent(float timespan, string eventName, int engineRepaired)
+            : base(timespan, eventName)
+        {
+            this.EngineRepaired = engineRepaired;
+        }
     }
 }
