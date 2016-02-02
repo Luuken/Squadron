6870517e0bc5c9c64337e106fb3524125dd81645
Bruno <bruno.suneson@gmail.com>
2016-02-02 15:25:46 +0100

    Worked more on error messages and events

--- Squadron5missing/Squadron5missing/Game1.cs
+++ Squadron5missing/Squadron5missing/Game1.cs
@@ -26,6 +26,10 @@ namespace Squadron5missing
         Mechanic mechanic;
         EngineEvent engineEvent;
 
+        Random rand;
+        List<string> alertList = new List<string>();
+        List<Event> alertListv2 = new List<Event>();
+
         Texture2D background;
         Texture2D repairKnapp;
         Texture2D matKnapp;
@@ -33,9 +37,11 @@ namespace Squadron5missing
         Rectangle repairKnappRectangle;
         Rectangle matKnappRectangle;
         Rectangle sjukvårdsKnappRectangle;
-
+        string alertTemp;
+        ErrorMessage p;
         int gameSpeed = 1;
-
+        int temp;
+        bool writeEvent;
 
         public Game1()
         {
@@ -62,10 +68,10 @@ namespace Squadron5missing
             graphics.PreferredBackBufferHeight = 900;
             graphics.IsFullScreen = true;
             graphics.ApplyChanges();
-
+            rand = new Random();
             //Initializing characters
             mechanic = new Mechanic(Content.Load<Texture2D>("placeHolder"), new Vector2(1000, 100), RoomE.Bridge, "Morgan the Mechanic", 64, 128, 2, 5, 5, 5, 5, 5, "Olaf");
-
+            p = new ErrorMessage();
             //Initializing events
             engineEvent = new EngineEvent(200, "Engine broke down", clock, "The engines Fluxual Accelerate Perperator has been damaged and needs repair");
         }
@@ -111,17 +117,39 @@ namespace Squadron5missing
                 this.Exit();
             mechanic.Update(gameTime);
             
-            if (gameSpeed == 1)
+            clock = clock.AddMilliseconds(16.6666666666667 * gameSpeed);
+            
+            //setts random values to se if a random event should happen
+            temp = rand.Next(1, 50);
+            if (temp == 2)
             {
-                clock = clock.AddMilliseconds(16.6666666666667);
-            }
-            else if (gameSpeed == 2)
-            {
-                clock = clock.AddMilliseconds(16.6666666666667 * 2);
-            }
-            else if (gameSpeed == 3)
-            {
-                clock = clock.AddMilliseconds(16.6666666666667 * 3);
+                //binds random message too a string that is then put in a list
+                alertTemp = p.DrawText(spriteBatch, testFont, new Vector2(75, 75));
+                int tempD;
+                if (p.eventNumber == 1)
+	            {
+                        
+                    tempD = rand.Next(60,190);
+		            alertListv2.Add(new PilotEvent((double)tempD, "Pilot event", clock, alertTemp));
+	            }
+                else if (p.eventNumber == 2)
+	            {
+		            tempD = rand.Next(20,230);
+                    alertListv2.Add(new RadarEvent((double)tempD, "Radar event", clock, alertTemp));
+	            }
+                else if (p.eventNumber == 3)
+	            {
+		            tempD = rand.Next(90,110);
+                    alertListv2.Add(new InfermaryEvent((double)tempD, "Infermary event", clock, alertTemp));
+	            }
+                else if (p.eventNumber == 4)
+	            {
+		            tempD = rand.Next(50,200);
+                    alertListv2.Add(new EngineEvent((double)tempD, "Engine event", clock, alertTemp));
+	            }
+                
+                
+                //we should create events based on what string it is + random numbers for duration and such
             }
             
             //e.CurrentTime = clock;
@@ -146,6 +174,11 @@ namespace Squadron5missing
             spriteBatch.Draw(sjukvårdsKnapp, sjukvårdsKnappRectangle, Color.White);
             spriteBatch.Draw(repairKnapp, repairKnappRectangle, Color.White);*/
             
+            //early draft, draws the message byt not for as long as i would like nor can you use this for anything
+            if (temp == 2)
+            {
+                spriteBatch.DrawString(testFont, alertTemp, new Vector2(75, 75), Color.White);
+            }
             spriteBatch.End();
             // TODO: Add your drawing code here
 
